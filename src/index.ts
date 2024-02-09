9512ebe6ffd58d3aef4bfc9e4de7662da617fb42
Junos16 <hriddhitdatta2002@gmail.com>
2024-02-09 21:19:32 +0530

    Set up express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello");
+import express from 'express';
+
+const app = express();
+app.get('/', (_, res) => {
+    res.send("hello");
+})
+app.listen(4000, () => {
+    console.log('server started on localhost:4000')
+}).on('error', (err) => {
+    console.log(err.message)
+});
