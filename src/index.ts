30a056e948d58779799df621ebd18500fcb2127a
Junos16 <hriddhitdatta2002@gmail.com>
2024-02-09 21:31:05 +0530

    Set up apollo server with a graphql resolver

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import express from 'express';
+import { ApolloServer } from 'apollo-server';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
-const app = express();
-app.get('/', (_, res) => {
-    res.send("hello");
-})
-app.listen(4000, () => {
-    console.log('server started on localhost:4000')
-}).on('error', (err) => {
-    console.log(err.message)
+const main = async () => {
+    const app = express();
+
+    app.get('/', (_, res) => {
+        res.send("hello");
+    })
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
+
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000')
+    });
+}
+
+main().catch((err) => {
+    console.error(err);
 });
+
